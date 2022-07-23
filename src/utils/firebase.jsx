500c3da54b3948933014bba3f8a18c93628da254
Hasan <hasan.yilbat@gmail.com>
2022-07-23 16:13:31 +0300

    starter

---
+++ src/utils/firebase.jsx
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+
+import { getDatabase, ref, set } from "firebase/database";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  // ...
+  // The value of `databaseURL` depends on the location of the database
+  databaseURL:
+    "https://firecontactapp-35c1e-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+
+function writeUserData(userId, name, email, imageUrl) {
+  const db = getDatabase();
+  set(ref(db, "users/" + userId), {
+    username: name,
+    email: email,
+    profile_picture: imageUrl,
+  });
+}
